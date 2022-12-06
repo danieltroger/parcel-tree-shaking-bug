fbca02977362f603cfb862426674e05f32f78fe7
Daniel Troger <daniel@natur-kultur.eu>
2022-12-06 14:58:42 +0100

    Example parcel output that would fix tree shaking

--- packages/issue-reproduction-library/dist/module.js
+++ packages/issue-reproduction-library/dist/module.js
@@ -3,7 +3,7 @@ function $parcel$export(e, n, v, s) {
 }
 var $ed83c5b837cbf49e$exports = {};
 
-$parcel$export($ed83c5b837cbf49e$exports, "library_function", () => $ed83c5b837cbf49e$export$cae6fff0c631454f);
+/*@__PURE__*/ $parcel$export($ed83c5b837cbf49e$exports, "library_function", () => $ed83c5b837cbf49e$export$cae6fff0c631454f);
 function $b208948437c85312$export$f5e092cdfed98805(fn) {
     console.log("Nice, the error is wrapped! By the way, imagine there are kilobytes of functions like me");
     return function() {
@@ -16,7 +16,7 @@ function $b208948437c85312$export$f5e092cdfed98805(fn) {
 }
 
 
-const $ed83c5b837cbf49e$export$cae6fff0c631454f = /*@__PURE__*/ (0, $b208948437c85312$export$f5e092cdfed98805)(()=>{
+const $ed83c5b837cbf49e$export$cae6fff0c631454f = (0, /*@__PURE__*/ $b208948437c85312$export$f5e092cdfed98805)(()=>{
     console.log("I just did very complex stuff. Imagine there are very many functions like me!");
 });
 
